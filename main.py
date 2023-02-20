b590f9875b346f02c02d6ab4476568d307b71860
ajgutierrez <84606504+ajgutierr3z@users.noreply.github.com>
2023-02-19 21:42:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,125 +1,116 @@
-import logging
-import sys
 import tweepy
-import csv
-import re
+import pandas as pd
 from textblob import TextBlob
-from geopy.geocoders import Nominatim
+import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-logging.basicConfig(
-    stream=sys.stdout,
-    datefmt='%Y-%m-%d %H:%M',
-    format='%(asctime)s | %(levelname)s | %(message)s'
-)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
-def create_conect_twt(consumer_key: str, consumer_secret: str, access_token: str,
-                      access_token_secret: str, logger: logging.Logger) -> tweepy:
+def connect_to_twitter_api(api_key: str, api_secret_key: str, access_token: str, access_token_secret: str):
     """
-    Create conecction with twitter
-    :param consumer_key: key for consumer consults
-    :param consumer_secret: key secret for consumer
-    :param access_token: key for access
-    :param access_token_secret: key secrets for access
-    :param logger: Message of status
-    :return: tweepy.API(auth)
+    Función que realiza la conexión a la API de Twitter.
+
+    Args:
+    api_key (str): clave de API de Twitter.
+    api_secret_key (str): clave secreta de API de Twitter.
+    access_token (str): token de acceso de Twitter.
+    access_token_secret (str): token de acceso secreto de Twitter.
+
+    Returns:
+    Objeto tweepy.API: objeto API de Twitter autenticado.
     """
-    # Autentica a través de OAuth
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth = tweepy.OAuthHandler(api_key, api_secret_key)
     auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    logging.info('Conexión a la API de Twitter establecida')
+    return api
 
-    # Crea una instancia de la API de Tweepy
-    logger.info('Connection created success')
-    return tweepy.API(auth)
 
-
-def geolacalizacion_nombre(location: str, logger: logging.Logger) -> str:
+def extract_timeline(api: tweepy.API, screen_name: str, since_date: str, until_date: str, location: str, keyword: str):
     """
-    Create string for location with latitude, longitude and count of km
-    :param location: Location at calculate latitude and longitude
-    :param logger: Message of status
-    :return: geolocator
+    Función que extrae el timeline de un usuario de Twitter, filtrado por fechas, ubicación y palabras clave.
+
+    Args:
+    api (tweepy.API): objeto API de Twitter autenticado.
+    screen_name (str): nombre de usuario de Twitter del que se quiere extraer el timeline.
+    since_date (str): fecha de inicio del rango de fechas en formato YYYY-MM-DD.
+    until_date (str): fecha de fin del rango de fechas en formato YYYY-MM-DD.
+    location (str): ubicación de los tweets que se quieren extraer.
+    keyword (str): palabra clave a buscar en los tweets.
+
+    Returns:
+    lista de objetos Status: lista de tweets filtrados.
     """
-    # Crear el objeto geolocator
-    geolocator = Nominatim(user_agent="Consult_data_research")
+    tweets = []
+    logging.info("Preparando extracción de tweets")
+    for status in tweepy.Cursor(api.user_timeline, screen_name=screen_name, tweet_mode="extended").items():
+        if (status.created_at.date() >= pd.to_datetime(since_date).date() and
+            status.created_at.date() <= pd.to_datetime(until_date).date() or
+            location.lower() in status.user.location.lower() and
+            keyword.lower() in status.full_text.lower()):
+            tweets.append(status)
+    logging.info(f'Se han extraído {len(tweets)} tweets')
+    return tweets
 
-    # Obtener las coordenadas de la ciudad de México
-    location = geolocator.geocode(location)
 
-    # Crear una cadena con el formato necesario para la API de Twitter
-    geocode_str = f"{location.latitude}, {location.longitude}, 1000km"
-    logger.info('Create latitude, longitude with success')
-    return geocode_str
-
-
-def create_analytic_csv(name_file: str, name_user: str, date: str, location: str, limit_twt: int,
-                        search_words: list, api: tweepy, logger: logging.Logger):
+def analyze_sentiment(text: str):
     """
-    Create analytic of sentiment and result generated one csv with date of publication,
-    text of tweet, user, and sentiments
-    :param name_file: name of csv to create
-    :param name_user: name of timeline to extrac of tweet
-    :param date: date start to create to the extract
-    :param location: location for create la extract
-    :param limit_twt: limte of twt to consult
-    :param search_words: list of keyword to search in one timeline
-    :param api: conecction with twitter
-    :param logger: Message of status
+    Función que analiza el sentimiento de un texto usando TextBlob.
+
+    Args:
+    text (str): texto a analizar.
+
+    Returns:
+    float: polaridad del sentimiento del texto en el rango [-1, 1].
     """
-    geocode = geolacalizacion_nombre(location, logger)
+    blob = TextBlob(text)
+    return blob.sentiment.polarity
 
-    # Abre un archivo CSV para escribir los resultados
-    name_csv = name_file+'.csv'
-    logger.info('Created file: '+name_csv)
-    # Busca tweets que contengan la palabra clave "Python"
-    keyword = "Python"
-    filtered_tweets = [tweet for tweet in tweets if keyword in tweet.text]
 
-    with open(name_csv, mode='w', encoding='utf-8', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Fecha', 'Tweet', 'Usuario', 'Sentimiento'])
-        #print(geocode)
-        # Recorre la timeline del usuario y escribe los tweets en el CSV
-        for tweet in tweepy.Cursor(api.user_timeline, screen_name=name_user, tweet_mode='extended',
-                                   #geocode=geocode,
-                                   since_id=date).items(limit_twt):
-            tweet_text = tweet.full_text
-            if any(word in tweet.full_text for word in search_words):
-                cleaned_text = re.sub(r'http\S+', '', tweet_text)
-                cleaned_text = re.sub(r'RT.*', '', cleaned_text)
-                cleaned_text = cleaned_text.strip()
-                sentiment = TextBlob(cleaned_text).sentiment.polarity
-                if sentiment > 0:
-                    sentiment_label = 'Positivo'
-                elif sentiment < 0:
-                    sentiment_label = 'Negativo'
-                else:
-                    sentiment_label = 'Neutro'
-                writer.writerow([tweet.created_at, cleaned_text, tweet.user.screen_name, sentiment_label])
+def export_to_csv(tweets: list, output_file: str):
+    """
+    Función que exporta los tweets a un archivo CSV con las cabezeras correspondientes.
 
-    logger.info('file: ' + name_csv + ' created success')
+    Args:
+    tweets (list): lista de tweets a exportar.
+    output_file (str): ruta del archivo de salida.
+    """
+    data = [[tweet.created_at, tweet.user.screen_name, tweet.full_text, analyze_sentiment(tweet.full_text)] for tweet in tweets]
+    df = pd.DataFrame(data, columns=['Fecha de publicación', 'Usuario', 'Texto', 'Sentimiento'])
+    df.to_csv(output_file, index=False)
+    logging.info(f'Los tweets se han exportado a {output_file}')
+
+
+def main(api_key, api_secret_key, access_token, access_token_secret, screen_name, since_date, until_date, location, keyword, output_file):
+    """
+    Función principal que ejecuta el script completo.
+
+    Args:
+    api_key (str): clave de API de Twitter.
+    api_secret_key (str): clave secreta de API de Twitter.
+    access_token (str): token de acceso de Twitter.
+    access_token_secret (str): token de acceso secreto de Twitter.
+    screen_name (str): nombre de usuario de Twitter del que se quiere extraer el timeline.
+    since_date (str): fecha de inicio del rango de fechas en formato YYYY-MM-DD.
+    until_date (str): fecha de fin del rango de fechas en formato YYYY-MM-DD.
+    location (str): ubicación de los tweets que se quieren extraer.
+    keyword (str): palabra clave a buscar en los tweets.
+    output_file (str): ruta del archivo de salida.
+    """
+    api = connect_to_twitter_api(api_key, api_secret_key, access_token, access_token_secret)
+    tweets = extract_timeline(api, screen_name, since_date, until_date, location, keyword)
+    export_to_csv(tweets, output_file)
 
 
 if __name__ == '__main__':
-    # Nombre de usuario del usuario a analizar
-    user_name = 'elonmusk'
-    # fechas a buscar
-    date_since = "2000-01-01"
-    # Palabras clave a buscar
-    keywords = ['COVID-19', 'pandemia']
-    #volumen de twets
-    max_twets = 100
-    #localidad a buscar en unradio de 1000km
-    locations = 'Texas'
-    #npmbre dle csv
-    file = 'tweets'
-    # Define las llaves y tokens de autenticación, sustituya las equis ('x') por sus llaves de conexión a la api
-    consumer_key = 'xxxxxxxxxxxxxxxxx' 
-    consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    access_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    conect_twt = create_conect_twt(consumer_key, consumer_secret, access_token, access_token_secret, logger)
-    create_analytic_csv(file, user_name, date_since, locations, max_twets, keywords, conect_twt, logger)
+    api_key = 'M14QtIplcDW1PLDJYaQrFmQVq'
+    api_secret_key = 'tfjwzGS1MlY5DGLTPL2nYEm1km4n4G0MoRl0HlAthUwi4P3Zkq'
+    access_token = '176952989-Bb1vWWQbprc5Ass3Pt3YpApSzuixznz5e0QkmFgz'
+    access_token_secret = 'wF6uiCDWpoT6E3KImL2a2MgyE9hQKIB2tX8cAL8hwkic4'
+    screen_name = 'elonmusk'
+    since_date = '2000-01-01'
+    until_date = '2023-03-30'
+    location = 'California'
+    keyword = 'space'
+    output_file = 'tweets.csv'
+    main(api_key, api_secret_key, access_token, access_token_secret, screen_name, since_date, until_date, location, keyword, output_file)
